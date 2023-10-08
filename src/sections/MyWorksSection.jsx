b201f67fbae4736d4260073916d9e337ca2557fb
Andro <unan9997@gmail.com>
2023-10-08 17:29:44 +0400

    delete images

--- src/sections/MyWorksSection.jsx
+++ src/sections/MyWorksSection.jsx
@@ -2,8 +2,7 @@ import styles from '../styles/styles.module.css'
 import ProjectBox from '../components/ProjectBox'
 import uberImage from '../img/uber777.png'
 import gameTryImage from '../img/gameTry.png'
-import ImageGallery from "react-image-gallery"
-import { images } from '../data/data'
+
 
 const MyWorksSection = () => {
     return(
@@ -14,13 +13,6 @@ const MyWorksSection = () => {
                     <ProjectBox text="Uber - CSS/Bootstrap Grid" image={uberImage} link="https://androun.github.io/uberTry/" repository="https://github.com/AndroUn/uberTry"/>
                     <ProjectBox text="GameTry - Couple Javascript elements" image={gameTryImage} link="https://androun.github.io/gameTry/" repository="https://github.com/AndroUn/gameTry"/>
                 </div>
-
-{/* 
-                <div className={styles.imageGallery}>
-                <p className={styles.worksSectionP}>Graphic Works</p>
-                    <ImageGallery items={images}/>
-
-                </div> */}
             </div>
         </section>
     )
