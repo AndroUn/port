96f057be6db6e267ca9161a43bc5dc2ef269cf75
Andro <unan9997@gmail.com>
2023-10-30 18:30:41 +0400

    about fix

--- src/sections/AboutMeSection.jsx
+++ src/sections/AboutMeSection.jsx
@@ -15,14 +15,15 @@ const AboutMeSection = () => {
           <p className={styles.aboutHeader}>About Me</p>
           <div class={styles.box}>
               <div class={styles.about_descr}>
-                  I would really like to start working as a beginner front end developer, yes I don't have much experience, 
-                  but at the same time I am a fast learner and I am motivated to learn all the ins and outs of this profession and become better in it. 
+                  Hello, my name is Andro Unanov.
                   I graduated from the Georgian Technical University, where I learned the basics of programming and creating website layout, 
                   also had deal with databases and etc. 
                   I also have experience in editing videos, creating 3D objects and creating websites in Wix.
                   <br></br>
+                  Recently I have completed a Front-End Development course at Skillwill Academy, where I have learned React, Redux and basics of React Native.
                   <br></br>
-                  Recently I have completed a React/React-native course at Skillwill Academy.
+                  I would really like to start working as a beginner front end developer, yes I don't have much experience,
+                  but at the same time I am a fast learner and I am motivated to learn all the ins and outs of this profession and become better in it.
               </div>
           </div>
           <Link className={styles.contactBtn} to='footer' smooth={true} duration={500}>Contact</Link>
